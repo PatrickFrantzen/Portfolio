7c9b565a655fc8988865a8d8dccf8b3d9c912781
Patrick Frantzen <patrick.frantzen@web.de>
2022-12-24 08:30:43 +0100

    update

--- src/app/introducing/introducing.component.ts
+++ src/app/introducing/introducing.component.ts
@@ -6,6 +6,8 @@ export interface Tile {
   rows: number;
   text: string;
   size: number;
+  writingMode: string;
+  transform: any;
 }
 
 @Component({
@@ -15,9 +17,9 @@ export interface Tile {
 })
 export class IntroducingComponent {
   tiles: Tile[] = [
-    {text: 'I am', cols: 1, rows: 2, color: 'lightblue', size: 60},
-    {text: 'Patrick Frantzen', cols: 2, rows: 1, color: 'lightgreen', size: 60},
-    {text: 'FRONTEND DEVELOPER', cols: 2, rows: 1, color: 'lightpink', size: 60},
+    {text: 'I am', cols: 1, rows: 2, color: 'transparent', size: 60, writingMode: 'vertical-rl', transform: 'rotate(180deg)'},
+    {text: 'Patrick Frantzen', cols: 2, rows: 1, color: 'transparent', size: 60, writingMode: 'unset', transform: 'unset'},
+    {text: 'FRONTEND DEVELOPER', cols: 2, rows: 1, color: 'transparent', size: 60, writingMode: 'unset',transform: 'unset'},
   ]
 }
 //#141D2F
